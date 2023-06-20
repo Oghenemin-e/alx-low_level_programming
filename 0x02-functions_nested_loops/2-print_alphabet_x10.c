4e79a3283efb1a546677c1dc7b9a5a81281850e7
Oghenemin-e <lawalogheneminebabatunde@gmail.com>
2023-06-20 13:39:40 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,18 +3,17 @@
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
  */
+
 void print_alphabet_x10(void)
 {
 	int i;
-	char letter;
+	char alpha;
 
 	for (i = 0; i < 10; i++)
 	{
-		letter = 'a';
-		while (letter <= 'z')
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
-			_putchar(letter);
-			letter++;
+			_putchar(alpha);
 		}
 	}
 
