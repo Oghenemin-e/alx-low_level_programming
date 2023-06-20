492974a881a39c66f7630c4cab07c96ed8ecbdfe
Oghenemin-e <lawalogheneminebabatunde@gmail.com>
2023-06-20 15:08:10 -0700

    Fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,10 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li, %li, ", a, b);
+		printf("%li", a);
+		printf(", ");
+		printf("%li", b);
+
 		a += b;
 		b += a;
 	}
