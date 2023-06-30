7edaf25b0d4fe5d940787280107b008b3aa5a8a9
Oghenemin-e <lawalogheneminebabatunde@gmail.com>
2023-06-29 19:11:09 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,38 @@
+#Include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: On success, the character written
+ *         On error, -1 is returned
+ */
+int _putchar(char c)
+{
+	return (putchar(c));
+}
+
+/**
+ * print_number - Prints an integer
+ * @n: The integer to be printed
+ */
+void print_number(int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
