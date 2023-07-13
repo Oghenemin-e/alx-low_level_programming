36273c9f51a3485053946f8613ea4f11cf2f4af1
Oghenemin-e <lawalogheneminebabatunde@gmail.com>
2023-07-13 15:07:00 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,27 +1,22 @@
-#include "main.h"
+#include <stdlib.h>
 
 /**
- * _calloc - allocates memory using malloc, and initializes it to zero
- * @size: size of the memory block to be allocated
+ * _calloc - allocate memory for an array
  * @nmemb: number of elements
- *
- * Return: pointer to the address of the memory block
+ * @size: size of element in bytes
+ * Return: pointer to allocated memory
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *block;
-    unsigned int i;
+	char *p;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
-    block = malloc(nmemb * size);
-    if (block != NULL)
-    {
-        for (i = 0; i < (nmemb * size); i++)
-            block[i] = 0;
-        return (block);
-    }
-    else
-        return (NULL);
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+	nmemb *= size;
+	while (nmemb--)
+		p[nmemb] = 0;
+	return ((void *)p);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,10 +5,6 @@
 #include <stdlib.h>
 #define min(x, y) (((x) < (y)) ? (x) : (y))
 
-int multiply(int num1, int num2)
-int main(int argc, char *argv[])
-int isNumber(const char *str)
-
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
